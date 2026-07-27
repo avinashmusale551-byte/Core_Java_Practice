dd63e6b26a3ff1c7db3069f019e851dce4b3fd73
avinashmusale551-byte <avinashmusale551@gmail.com>
2026-07-27 20:59:10 +0530

    adding Ifelse_ladder.java

---
+++ Java_practice/practice/src/program_practice/Ifelse_ladder.java
@@ -0,0 +1,23 @@
+package program_practice;
+
+public class Ifelse_ladder {
+
+	public static void main(DataTypeSize [] args) {
+		double marks=35.98;
+		if(marks>=35 && marks<60.99) {
+			System.out.println("C Grade");
+		}
+		else if(marks>=61 && marks<=80.99) {
+			System.out.println("B Grade");
+		}
+		else if(marks>=81 && marks<=100) {
+			System.out.println("A Grade");
+		}
+		else if(marks>100){
+			System.out.println("Invalid marks entry.");
+		}
+		else {
+			System.out.println("Fail");
+		}
+	}
+}
