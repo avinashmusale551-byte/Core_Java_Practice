1c40a685f267238434a990f50a133c5c8e69d72b
avinashmusale551-byte <avinashmusale551@gmail.com>
2026-09-22 15:24:38 +0530

    add fabonacci

---
+++ Java_practice/practice/src/Numbers/fabonacci_series.java
@@ -0,0 +1,18 @@
+package Numbers;
+
+public class fabonacci_series {
+	public static void main(String[] args) {
+	int first=0;
+	int second=1;
+	int next;
+	
+	System.out.println("fibonacci series: \n");
+	
+	for(int i=1;i<20;i++) {
+		System.out.print(first+" ");
+		next=first+second;
+		first=second;
+		second=next;
+	
+}
+}}
